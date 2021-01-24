f03ff19a08a7eed31678743d33e7aa97f8dda213
Callum <callum@netzstrategen.com>
2021-01-24 01:25:28 +0000

    Added header logo.

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import Logo from './logo';
 
 const Header: React.FC = () => (
-  <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+  <h2 className="flex items-center text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <Logo className="mr-3" />
     <Link href="/">
       <a className="hover:underline">Bloke</a>
     </Link>
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Intro: React.FC = () => (
   <section className="flex-col md:flex-row flex md:items-center md:justify-between mt-16 mb-16 md:mb-12">
     <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-      Blog.
+      Bloke.
     </h1>
     <h4 className="md:text-left text-lg mt-5 md:pl-8">
       My travel blog from many{' '}
