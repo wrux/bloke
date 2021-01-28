8acf444f99ab256441e0e1330eb351685065f599
Callum <callum@netzstrategen.com>
2021-01-28 23:21:42 +0000

    Disabled image grid preview in Sanity.

--- studio/schemas/blocks/imageGrid.tsx
+++ studio/schemas/blocks/imageGrid.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import Component from '../../../components/blocks/imageGrid';
-
 export default {
   name: 'imageGrid',
   type: 'object',
@@ -33,23 +30,13 @@ export default {
       images: 'images',
     },
     prepare(selection) {
-      return { media: selection.images };
-    },
-
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    // eslint-disable-next-line react/display-name
-    component: ({ value }): React.JSX => {
-      const { media } = value;
-
-      if (!media) {
-        return (
-          <p style={{ padding: '1.125rem', margin: 0 }}>
-            Add images to see a preview
-          </p>
-        );
-      }
-
-      return <Component node={{ images: media }} />;
+      // TODO: create a functional image preview component.
+      const { images } = selection;
+      return {
+        ...selection,
+        title: 'Image grid',
+        subtitle: images ? `${images.length} images added.` : 'No images.',
+      };
     },
   },
 };
