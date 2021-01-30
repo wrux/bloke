a86c65c0d05754f0c304cc7b43eb01d712a37d77
Callum <callum@netzstrategen.com>
2021-01-30 21:47:52 +0000

    pdate Tailwind to version 2.

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -10,7 +10,7 @@ const MoreStories: React.FC<Props> = ({ posts }) => (
     <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
       More Stories
     </h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 md:gap-24 mb-32">
       {posts.map((post) => (
         <PostPreview
           key={post.slug}
