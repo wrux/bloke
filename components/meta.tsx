8e5488a5c910a5988eb5ede8531e06fa38ac13b0
Callum <callum@netzstrategen.com>
2021-02-10 22:04:38 +0000

    Added SVG favicon.

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-import { HOME_OG_IMAGE_URL } from '@lib/constants';
 
 const Meta: React.FC = () => (
   <Head>
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicon/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon/favicon-16x16.png"
-    />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
-    <link
-      rel="mask-icon"
-      href="/favicon/safari-pinned-tab.svg"
-      color="#000000"
-    />
-    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <meta name="theme-color" content="#000" />
-    <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
     <meta
       name="description"
-      content="A statically generated blog example using Next.js and Sanity.io."
+      content="My travel blog from many Countries all around the world."
     />
-    <meta property="og:image" content={HOME_OG_IMAGE_URL} />
   </Head>
 );
 
