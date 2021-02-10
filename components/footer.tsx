932673f6f6c79d8caaca9933808005901771eb45
Callum <callum@netzstrategen.com>
2021-02-10 23:34:30 +0000

    fixup! Added work element to footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,18 +6,16 @@ const Footer: React.FC = () => (
   <footer className="bg-accent-1 border-t border-accent-2">
     <Work />
     <Container>
-      <div className="py-28 flex flex-col lg:flex-row items-center">
-        <h3 className="text-4xl lg:text-5xl font-bold tracking-tight leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+      <div className="py-28 flex flex-col md:flex-row items-center md:justify-around gap-8">
+        <p className="text-4xl md:text-5xl font-bold tracking-tight leading-tight text-center md:text-left mb-10 md:mb-0 md:pr-4">
           Like this website?
-        </h3>
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-          <a
-            href="https://github.com/wrux/bloke"
-            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-          >
-            View the source code
-          </a>
-        </div>
+        </p>
+        <a
+          href="https://github.com/wrux/bloke"
+          className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 md:px-8 duration-200 transition-colors mb-6 md:mb-0 text-center"
+        >
+          View the source code
+        </a>
       </div>
     </Container>
   </footer>
