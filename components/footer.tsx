68bec8e12a24eefed278d106310ef7574bb43836
Callum <callum@netzstrategen.com>
2021-02-10 23:21:38 +0000

    Added work element to footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from './container';
+import Work from './work';
 
 const Footer: React.FC = () => (
   <footer className="bg-accent-1 border-t border-accent-2">
+    <Work />
     <Container>
       <div className="py-28 flex flex-col lg:flex-row items-center">
-        <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+        <h3 className="text-4xl lg:text-5xl font-bold tracking-tight leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
           Like this website?
         </h3>
         <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
